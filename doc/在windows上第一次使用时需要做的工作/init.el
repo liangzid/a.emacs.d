3543c1c24f11bdb949dadb8fbcf0d7f21355dede
liangzid <2273067585@qq.com>
2020-09-20 08:49:54 +0800

    增加了一键配置的脚本

---
+++ doc/在windows上第一次使用时需要做的工作/init.el
@@ -0,0 +1,27 @@
+;;HAHA!This is the first config file of LIANGZI!
+
+;;for Windows System.Change the path of HOME.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setenv "HOME" "H:/")
+(load "~/.emacs.d/init.el")
+
+ 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
---
+++ doc/在windows上第一次使用时需要做的工作/readme.txt
@@ -0,0 +1,4 @@
+1. find path "C:\user(用户)\用户名（如22730）\AppData\Roaming\";
+2. mkdir dir called ".emacs.d"
+3. new file called "init.el", which is in this directory.
+4. Please read the init.el which will set the "home path",which is important for you.
