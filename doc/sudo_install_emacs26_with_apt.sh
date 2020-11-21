a2ae2b5266d9f8b4aeeb29259e744c2025ffb9fe
liangzid <2273067585@qq.com>
2020-11-21 15:06:30 +0800

    add the document of how to download emacs26

---
+++ doc/sudo_install_emacs26_with_apt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# add ppa
+sudo add-apt-repository ppa:kelleyk/emacs
+
+# install emacs26
+sudo apt update
+sudo apt install emacs26
+
+# if no GUI, you can use
+# sudo apt install emacs26-nox
+
+# if remove it, you can use
+# sudo apt remove --autoremove emacs26 emacs26-nox
