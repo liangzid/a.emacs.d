dde521a59aa8036541d34bdd94f9b5769682ed2b
18842378119 <2273067585@qq.com>
2021-05-08 17:03:55 +0800

    change the shunxu of init-load...

--- init.el
+++ init.el
@@ -4,21 +4,20 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 
-
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-(load-file custom-file)
-
 (require 'init-elpa)
 (require 'init-self-config-function)
-(require 'init-ui)
 (require 'init-python)
-(require 'init-better-defaults)
-(require 'init-keybindings)
 (require 'init-org)
 (require 'init-latex)
 (require 'init-cpp)
 (require 'init-blog)
+
+(require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-keybindings)
+
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
 
 (require 'init-packages)
 
