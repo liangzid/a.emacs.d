4479609f291c35e3af0cd19337b545ea5e37775e
liangzid <2273067585@qq.com>
2022-03-08 12:13:59 +0800

    add new doc

---
+++ doc/3.gitclone.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##############################################################################################
+#3.GITCLONE --- 
+
+# Author: Zi Liang <liangzid@stu.xjtu.edu.cn>
+# Copyright © 2022, ZiLiang, all rights reserved.
+# Created:  8 March 2022
+##############################################################################################
+
+################################### Commentary ###############################################
+##  
+##############################################################################################
+
+
+git clone https://github.com/liangzid/easy-collections.git
+
+git clone https://github.com/SpringHan/netease-cloud-music.el.git
+
+
+
+
+
+
+
+
+
+echo "RUNNING 3.gitclone.sh DONE."
+# 3.gitclone.sh ends here
