ead7bb938aa2af8af2dcd09f272977bb7c0d52e6
liangzid <2273067585@qq.com>
2023-10-04 09:26:48 +0800

    add initial configs of new OS.

---
+++ doc/6.initial_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+######################################################################
+#6.INITIAL_SETUP --- 
+
+# Author: Zi Liang <liangzid@stu.xjtu.edu.cn>
+# Copyright © 2023, ZiLiang, all rights reserved.
+# Created:  4 October 2023
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+## git related
+
+# add git basic configuration
+git config --global user.email 2273067585@qq.com
+git config --global user.name liangzid
+
+# add auto store password of git.
+git config --global credential.helper store
+
+echo "RUNNING 6.initial_setup.sh DONE."
+# 6.initial_setup.sh ends here
