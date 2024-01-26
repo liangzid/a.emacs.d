fa3f7be28b73ccad30f4dfe4e6638f21ba824132
liangzid <2273067585@qq.com>
2024-01-26 15:37:46 +0800

    update the cloning things.

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,8 +1,9 @@
 ;;(require 'init-elpa)
 (my/install-package-if-not-found 'rust-mode)
 (my/install-package-if-not-found 'rustic)
-(require 'rust-mode)
-(require 'rustic)
+;; (require 'rust-mode)
+;; (require 'rustic)
+
 ;; (require 'use-package)
 
 (provide 'init-rust)
