6c4609a5628c434e7fcf35376e99d615e046b9f9
liangzid <2273067585@qq.com>
2024-03-07 20:36:17 +0800

    boring settings.

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
 (setq gc-cons-threshold most-positive-fixnum)
 (setq gc-cons-threshold (* 50 1024 1024)) 
 
-;; (set-frame-parameter nil 'alpha 0.85)
-(set-frame-parameter nil 'alpha 0.99999)
+(set-frame-parameter nil 'alpha 0.9)
+;; (set-frame-parameter nil 'alpha 0.99999)
 
 ;; 定义快速打开init-file的操作，并将之绑定在F2上.
 (defun open-my-init-file()
